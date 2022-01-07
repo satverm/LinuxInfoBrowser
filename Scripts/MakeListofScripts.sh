6ef150b7d449d2d4787cb9348ac64ccafc21ca81
satverm <64876484+satverm@users.noreply.github.com>
2022-01-07 19:10:18 +0530

    Update and rename MakeListofScripts.txt to MakeListofScripts.sh

--- Scripts/MakeListofScripts.sh
+++ Scripts/MakeListofScripts.sh
@@ -1,2 +1,2 @@
 InfoBrw: This is the script which generates the list of scripts contained in the scripts folder and exports to the file ScriptsUsed.org
-At present it is  text file but will later be converted to a scrip.
+This is the example of automation in generation of information from the linux file structure.
