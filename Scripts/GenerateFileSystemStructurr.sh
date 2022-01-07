ca3c0d894d9ca161d1f2882c2397fcdab50de0d2
satverm <64876484+satverm@users.noreply.github.com>
2022-01-07 14:54:54 +0530

    Create GenerateFileSystemStructurr.sh
    Created the second script file

---
+++ Scripts/GenerateFileSystemStructurr.sh
@@ -0,0 +1,5 @@
+#InfoBrw: This script generates the backbone of the file system and assigns a code for each file which can be used latefor following tasks:
+# Providing information about the file
+# Linking this code with other files as per their relationships.
+# Assigning a type like binary,configuration, directory(is also a file), script, image etc
+# Hashvalue: can be used in future for security applications
