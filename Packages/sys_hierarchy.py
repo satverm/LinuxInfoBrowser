0ab3efa71905f2e582bdc82f49d7160484fbcd49
satverm <64876484+satverm@users.noreply.github.com>
2022-12-29 17:34:35 +0530

    Update sys_hierarchy.py

--- Packages/sys_hierarchy.py
+++ Packages/sys_hierarchy.py
@@ -6,3 +6,10 @@ System attributes: SystemName, SystemID, SystemLevel, SystemDataType etc
 System functions: CreateSystem, CreateSubSystems(one or many as per rerquirement),ModifySystem, SelectSystem(can be used to select a system or subsystem),AddSystem(This can use the create system function),DeleteSystem(this will first use the selectsystem function)
 Class functions: These can be used for various tasks not directly related with system hierarchy but storage of data related to any sytemhierarchy. CreateDatabasefile, create tables, update database based on the changes done in a system. Thus the System functions would call class functions as per requriement. In fact creation of databae files and related tables as per requirement should be done whenever any new system is created and the user should provide inputs or defaults to be used.
 '''
+class System:
+  def __init__(self, sys_id = None, sys_name = None):
+    if sys_id == None:
+      self.sys_id = sys_id
+    if sys_name == None:
+      self.sys_name = sys_name
+  
